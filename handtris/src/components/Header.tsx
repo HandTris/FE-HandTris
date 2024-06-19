aa4cf6df6352df8bf4d11adcddce312584d38c62
bishoe01 <bishoe01@gmail.com>
2024-06-19 23:47:24 +0900

    [🧹 REFACTORING] OAUTH 타입에러 수정

--- handtris/src/components/Header.tsx
+++ handtris/src/components/Header.tsx
@@ -7,10 +7,7 @@ async function Header() {
   const session = await getServerSession(authOptions);
 
   return (
-    <header
-      className="flex fixed top-0 left-0 w-full items-center justify-between p-4 text-white border-b-2 border-green-500
-    gradient-to-r from-green-400 to-blue-500 shadow-md z-10"
-    >
+    <header className="flex fixed top-0 left-0 w-full items-center justify-between p-4 text-white border-b-2 border-green-500 gradient-to-r from-green-400 to-blue-500 shadow-md z-10">
       <nav className="flex space-x-4">
         <Link href="/" className="hover:text-green-500">
           Home
@@ -24,7 +21,7 @@ async function Header() {
       <div className="flex items-center space-x-4">
         {session ? (
           <>
-            <span className="text-sm">{session?.user.name}</span>
+            <span className="text-sm">{session.user?.name}</span>
             <Link href="/api/auth/signout" className="hover:text-green-500">
               Logout
             </Link>
