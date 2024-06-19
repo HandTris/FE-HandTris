b7d03da3e9edbfdc32e6f6d283ffd945ae464f77
bishoe01 <bishoe01@gmail.com>
2024-06-19 23:17:36 +0900

    [🪽 feat ] : HEADER 컴포넌트 로그인 관리

---
+++ handtris/src/app/not-found.tsx
@@ -0,0 +1,11 @@
+"use client";
+function NotFound() {
+  return (
+    <div>
+      <h1>Page Not Found</h1>
+      <p>The requested page could not be found.</p>
+    </div>
+  );
+}
+
+export default NotFound;
