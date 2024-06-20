a8fd68b8eeb48e35025c3b3e2bd176f0fdd4155c
bishoe01 <bishoe01@gmail.com>
2024-06-21 00:25:33 +0900

    [♻️ CHORE] Tailwind CSS 설정에 블러 효과, 유리 배경색, 사용자 정의 색상 추가

--- handtris/tailwind.config.ts
+++ handtris/tailwind.config.ts
@@ -8,8 +8,22 @@ const config: Config = {
     ],
     theme: {
         extend: {
+            backdropFilter: {
+                'none': 'none',
+                'blur': 'blur(20px)',
+            },
+            backgroundColor: {
+                'glass': 'rgba(255, 255, 255, 0.3)',
+            },
             fontSize: {
                 base: ['1rem', { lineHeight: '1.5', letterSpacing: '0' }],
+            },
+            colors: {
+                'green400': '#1D3E2A',
+                'green300': "#4AA645",
+                'green200': "#51C063",
+                'green100': "#88FF99"
+
             }
         },
     },
