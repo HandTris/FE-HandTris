88ae08838a1045a1649195df1e298e22a824347b
조승우 <choseung97@gmail.com>
2024-07-02 20:53:13 +0900

    [🎨 UI/UX] #58 게임 종료 시 동시에 canvas 초기화

--- handtris/src/app/play/tetris/page.tsx
+++ handtris/src/app/play/tetris/page.tsx
@@ -98,14 +98,32 @@ const Home: React.FC = () => {
       subscribeToState();
     }
   }, [isOwner]);
-  // Add useEffect to handle the timeout for hiding gameResult
+  
+  // 게임 종료 시 결과 표시 모달 지우고, 게임 시작 관련 상태 초기화
   useEffect(() => {
     if (gameResult) {
       const timeoutId = setTimeout(() => {
         setGameResult(null);
         setIsStart(false);
         setIsAllReady(false);
-      }, 3000); // 3 seconds
+        setLinesCleared(0);
+        setGauge(0);
+        if (tetrisGameRef.current) {
+          tetrisGameRef.current.linesCleared = 0;
+        }
+        if (canvasTetrisRef.current) {
+          const ctx = canvasTetrisRef.current.getContext("2d");
+          if (ctx) {
+            ctx.clearRect(0, 0, canvasTetrisRef.current.width, canvasTetrisRef.current.height);
+          }
+        }
+        if (canvasTetris2Ref.current) {
+          const ctx2 = canvasTetris2Ref.current.getContext("2d");
+          if (ctx2) {
+            ctx2.clearRect(0, 0, canvasTetris2Ref.current.width, canvasTetris2Ref.current.height);
+          }
+      }
+    }, 3000); // 3 seconds
 
       return () => clearTimeout(timeoutId);
     }
