b85177b2708a958ac9d99057b9e5d5226c293a06
bishoe01 <bishoe01@gmail.com>
2024-07-02 14:47:35 +0900

    [🐛 FIX] build 에러 수정

--- handtris/src/components/ThreeScene.tsx
+++ handtris/src/components/ThreeScene.tsx
@@ -1,13 +1,13 @@
-import * as THREE from 'three';
-import { useRef, useEffect, useState } from 'react';
-import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader';
+import * as THREE from "three";
+import { useRef, useEffect, useState } from "react";
+import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader.js";
 
-const ThreeScene = ({ handLandmarks }) => {
-  const mountRef = useRef(null);
-  const sceneRef = useRef(null);
-  const cameraRef = useRef(null);
-  const rendererRef = useRef(null);
-  const joystickRef = useRef(null);
+const ThreeScene = ({ handLandmarks }: { handLandmarks: any }) => {
+  const mountRef = useRef<HTMLDivElement>(null);
+  const sceneRef = useRef<THREE.Scene | null>(null);
+  const cameraRef = useRef<THREE.PerspectiveCamera | null>(null);
+  const rendererRef = useRef<THREE.WebGLRenderer | null>(null);
+  const joystickRef = useRef<THREE.Object3D | null>(null);
   const [cameraAdjusted, setCameraAdjusted] = useState(false);
 
   useEffect(() => {
@@ -22,7 +22,7 @@ const ThreeScene = ({ handLandmarks }) => {
       0.1,
       1000
     );
-    const renderer = new THREE.WebGLRenderer({ antialias: true , alpha: false});
+    const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: false });
     renderer.setSize(mount.clientWidth, mount.clientHeight);
     mount.appendChild(renderer.domElement);
 
@@ -30,7 +30,7 @@ const ThreeScene = ({ handLandmarks }) => {
     const ambientLight = new THREE.AmbientLight(0x404040, 2); // Soft white light
     scene.add(ambientLight);
     const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
-    directionalLight.position.set(0, 250,0).normalize();
+    directionalLight.position.set(0, 250, 0).normalize();
     scene.add(directionalLight);
 
     // Axes Helper
@@ -41,14 +41,14 @@ const ThreeScene = ({ handLandmarks }) => {
     const loader = new OBJLoader();
     const objUrlPlate = "/image/joystickplate.obj";
     const objUrlBall = "/image/joystickball2.obj";
-    
+
     // 판
     loader.load(
       objUrlPlate,
       (obj) => {
         obj.traverse((node) => {
-          if (node.isMesh) {
-            node.material = new THREE.MeshPhongMaterial({
+          if ((node as THREE.Mesh).isMesh) {
+            (node as THREE.Mesh).material = new THREE.MeshPhongMaterial({
               color: 0x3c3c3c,
               emissive: 0x111111, // Add emissive property for better visibility
             });
@@ -58,21 +58,21 @@ const ThreeScene = ({ handLandmarks }) => {
         obj.scale.set(0.15, 0.15, 0.15); // Scale the model down
         obj.position.set(2.5, 4, 0.5); // Set initial position
         scene.add(obj);
-        console.log('Joystick model loaded:', obj);
+        console.log("Joystick model loaded:", obj);
       },
       undefined,
       (error) => {
-        console.error('An error occurred while loading the model', error);
+        console.error("An error occurred while loading the model", error);
       }
     );
-    
+
     // 대+Ball
     loader.load(
       objUrlBall,
       (obj) => {
         obj.traverse((node) => {
-          if (node.isMesh) {
-            node.material = new THREE.MeshPhongMaterial({
+          if ((node as THREE.Mesh).isMesh) {
+            (node as THREE.Mesh).material = new THREE.MeshPhongMaterial({
               color: 0xff0000,
               emissive: 0x333333, // Add emissive property for better visibility
             });
@@ -83,14 +83,14 @@ const ThreeScene = ({ handLandmarks }) => {
         obj.position.set(0, 0, -10); // Set initial position
         scene.add(obj);
         joystickRef.current = obj;
-        console.log('Joystick model loaded:', obj);
+        console.log("Joystick model loaded:", obj);
 
         // Adjust camera to fit the model
         const box = new THREE.Box3().setFromObject(obj);
 
         const radius = 30; // Distance from the center
         const angleInRadians = THREE.MathUtils.degToRad(30); // Convert 30 degrees to radians
-        const x = 0
+        const x = 0;
         const y = radius * Math.cos(angleInRadians);
         const z = 60;
 
@@ -103,7 +103,7 @@ const ThreeScene = ({ handLandmarks }) => {
       },
       undefined,
       (error) => {
-        console.error('An error occurred while loading the model', error);
+        console.error("An error occurred while loading the model", error);
       }
     );
 
@@ -136,23 +136,24 @@ const ThreeScene = ({ handLandmarks }) => {
       const angle = -Math.atan2(deltaZ, deltaX); // theta, 단위: rad
 
       // Rotate the joystick model based on the calculated angle
-      if (angle > 1.05 && angle < 2.1){ // 60도 이상 120도 이하 움직임 제한
+      if (angle > 1.05 && angle < 2.1) {
+        // 60도 이상 120도 이하 움직임 제한
         joystick.rotation.z = angle;
       }
 
       // Adjust camera only if it hasn't been adjusted yet
-      if (!cameraAdjusted) {
+      if (!cameraAdjusted && cameraRef.current) {
         const box = new THREE.Box3().setFromObject(joystick);
         const boxCenter = box.getCenter(new THREE.Vector3());
 
         const radius = 30; // Distance from the center
         const angleInRadians = THREE.MathUtils.degToRad(30); // Convert 30 degrees to radians
-        const x = 0
+        const x = 0;
         const y = radius * Math.cos(angleInRadians);
         const z = 60;
 
         cameraRef.current.position.set(x, y, z); // Set camera position
-        cameraRef.current.lookAt(new THREE.Vector3(0,0,0)); // Look at the center of the scene
+        cameraRef.current.lookAt(new THREE.Vector3(0, 0, 0)); // Look at the center of the scene
         cameraRef.current.updateProjectionMatrix();
 
         // Mark camera as adjusted
@@ -161,7 +162,7 @@ const ThreeScene = ({ handLandmarks }) => {
     }
   }, [handLandmarks, cameraAdjusted]);
 
-  return <div ref={mountRef} style={{ width: '320px', height: '240px' }} />;
+  return <div ref={mountRef} style={{ width: "320px", height: "240px" }} />;
 };
 
 export default ThreeScene;
