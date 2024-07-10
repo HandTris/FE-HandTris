c655462360b005741fd3f736a6dae6171bd0ac57
bishoe01 <bishoe01@gmail.com>
2024-07-10 19:53:07 +0900

    [🪽 FEAT]  BANNER 디자인 및 방 새로고침 버튼 생성

--- handtris/next.config.mjs
+++ handtris/next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false, // Strict Mode 해제
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "handtris.s3.ap-northeast-2.amazonaws.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
